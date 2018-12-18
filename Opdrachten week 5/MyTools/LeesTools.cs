7f7044ba90021073b9410b7a70b2566f3a10d67d
Unknown <s.grooff@student.tudelft.nl>
2018-12-18 22:29:30 +0100

    week 5

---
+++ Opdrachten week 5/MyTools/LeesTools.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTools
+{
+    public class LeesTools
+    {
+        public static int LeesInt(string vraag)
+        {
+            Console.Write(vraag);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static int LeesInt(string vraag, int min, int max)
+        {
+            int leeftijd;
+            do
+            {
+                leeftijd = LeesInt(vraag);
+            } while (leeftijd < min || leeftijd > max);
+            return leeftijd;
+        }
+
+        public static string LeesString(string vraag)
+        {
+            Console.WriteLine(vraag);
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ Opdrachten week 5/MyToolsApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using MyTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToolsApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            int getal = LeesTools.LeesInt("tik een getal: ");
+            Console.WriteLine("Je hebt {0} ingetikt.", getal);
+            int leeftijd = LeesTools.LeesInt("hoe oud ben je? ", 0, 120);
+            Console.WriteLine("Je bent {0} jaar oud.", leeftijd);
+            string naam = LeesTools.LeesString("Hoe heet je? ");
+            Console.WriteLine("Aangenaam kennis met je te maken, {0}.", naam);
+            Console.ReadKey();
+        }
+    }
+}
