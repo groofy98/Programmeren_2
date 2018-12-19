8715abc530ad0a68a5ec1b5c8bdf484ecf03e22f
Unknown <s.grooff@student.tudelft.nl>
2018-12-19 01:14:05 +0100

    week 5 kinda done

---
+++ Opdrachten week 5/CandyCrush/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CandyCrushLogica.Class1;
+
+namespace CandyCrush
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+            RegularCandies[,] speelveld;
+
+            try
+            {
+                speelveld = LeesSpeelveld("speelvelden");
+                SchrijfSpeelveld(speelveld, "speelvelden");
+            }
+            catch
+            {
+                Console.WriteLine("Nieuw speelveld wordt aangemaakt");
+                speelveld = new RegularCandies[9, 9];
+                InitCandies(ref speelveld);
+                SchrijfSpeelveld(speelveld, "speelvelden");
+            }
+            PrintCandies(speelveld);
+            if (ScoreRijAanwezig(speelveld))
+                Console.WriteLine("Score rij aanwezig!");
+            if (ScoreKolomAanwezig(speelveld))
+                Console.WriteLine("Score Kolom aanwezig!");
+            Console.ReadKey();
+            Start();
+        }
+
+        void SchrijfSpeelveld(RegularCandies[,] speelveld, string bestandsnaam)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(@"..\\..\\" + bestandsnaam + ".txt");
+            try
+            {
+                file.WriteLine(speelveld.GetLength(0));
+                file.WriteLine(speelveld.GetLength(1));
+                int width = speelveld.GetLength(0);
+                int height = speelveld.GetLength(1);
+                for (int i = 0; i < height; i++)
+                {
+                    string temp = "";
+                    for (int b = 0; b < width; b++)
+                    {
+                        temp += (int)speelveld[b, i] + " ";
+                    }
+                    file.WriteLine(temp);
+                }
+                file.Close();
+
+            }
+            finally
+            {
+                file.Close();
+            }
+        }
+
+        RegularCandies[,] LeesSpeelveld(string bestandsnaam)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(@"..\\..\\" + bestandsnaam + ".txt");
+            try
+            {
+                string[] line;
+                RegularCandies[,] speelveld = new RegularCandies[int.Parse(file.ReadLine()), int.Parse(file.ReadLine())];
+
+                for (int i = 0; i < speelveld.GetLength(1); i++)
+                {
+                    line = file.ReadLine().Split(' ');
+                    for (int b = 0; b < speelveld.GetLength(0); b++)
+                    {
+                        speelveld[b, i] = (RegularCandies)int.Parse(line[b]);
+                    }
+                }
+                file.Close();
+                return speelveld;
+            }
+            finally
+            {
+                file.Close();
+
+            }
+
+
+        }
+
+        void InitCandies(ref RegularCandies[,] speelveld)
+        {
+            Random r = new Random();
+            int width = speelveld.GetLength(0);
+            int height = speelveld.GetLength(1);
+            for (int i = 0; i < height; i++)
+            {
+                for (int b = 0; b < width; b++)
+                {
+                    speelveld[b, i] = (RegularCandies)r.Next(0, 6);
+                }
+            }
+        }
+
+        void PrintCandies(RegularCandies[,] speelveld)
+        {
+            int width = speelveld.GetLength(0);
+            int height = speelveld.GetLength(1);
+            for (int i = 0; i < height; i++)
+            {
+                for (int b = 0; b < width; b++)
+                {
+                    switch (speelveld[b, i])
+                    {
+                        case RegularCandies.GumSquare:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                        case RegularCandies.JellyBean:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            break;
+                        case RegularCandies.JujubeCluster:
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            break;
+                        case RegularCandies.LemonDrop:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+                        case RegularCandies.LollipopHead:
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            break;
+                        case RegularCandies.Lozenge:
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            break;
+                        default:
+                            Console.ResetColor();
+                            break;
+                    }
+                    Console.Write("# ");
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        
+    }
+}
---
+++ Opdrachten week 5/Lingo/Program.cs
@@ -0,0 +1,123 @@
+﻿using MyTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+            List<string> words = ReadWords();
+            string lingoWord = ChooseWord(words);
+            PlayLingo(lingoWord);
+            Start();
+        }
+
+        List<string> ReadWords()
+        {
+            List<string> l = new List<string>();
+            string line;
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"..\\..\\woorden.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Length >= 3)
+                    l.Add(line);
+            }
+
+            file.Close();
+
+            return l;
+        }
+
+        string ChooseWord(List<string> words)
+        {
+            Random r = new Random();
+            return words[r.Next(0, words.Capacity)];
+        }
+
+        void PlayLingo(string lingoWord)
+        {
+            int attempts = 5;
+            bool guessed = false;
+            do
+            {
+                string temp = ReadPlayerWord(lingoWord);
+                DisplayResult(temp, EvaluateWord(temp, lingoWord));
+                if (temp == lingoWord)
+                    guessed = true;
+                attempts--;
+            }
+            while (!guessed && attempts > 0);
+            if (guessed)
+                Console.WriteLine("gefeliciteerd");
+            else
+                Console.WriteLine("Helaas het woord was {0}", lingoWord);
+        }
+
+        int[] EvaluateWord(string playerWord, string lingoWord)
+        {
+            int[] outcome = new int[lingoWord.Length];
+            char[] pWord = playerWord.ToCharArray();
+            char[] lWord = lingoWord.ToCharArray();
+            for (int x = 0; x< lingoWord.Length; x++)
+            {
+                if (pWord[x] == lWord[x])
+                    outcome[x] = 1;
+                else if
+                    (lWord.Contains(pWord[x]))
+                    outcome[x] = 2;
+                else
+                    outcome[x] = 0;
+            }
+            return outcome;
+        }
+
+        void DisplayResult (string playerWord, int[] result)
+        {
+            char[] pWord = playerWord.ToCharArray();
+            for(int i = 0; i < pWord.Length; i++)
+            {
+                
+                switch (result[i])
+                {
+                    case 1:
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        break;
+                    case 2:
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        break;                    
+                }
+                Console.Write(pWord[i]);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+
+
+        }
+
+        string ReadPlayerWord(string word)
+        {
+            string temp;
+            temp = LeesTools.LeesString("Voer een woord in van " + word.Length + " letters: " );
+            if (temp.Length == word.Length)
+                return temp;
+            else
+                return ReadPlayerWord(word);
+        }
+    }
+}
